5f8e214929d6bb45b9db3e8b5ed512f5fc65512c
Zoey Zheng <zoeyyandi@gmail.com>
2018-03-21 13:45:34 -0400

    changed initial lon

--- client/src/features/app/app.reducers.js
+++ client/src/features/app/app.reducers.js
@@ -1,18 +1,22 @@
-import {APP_TYPES} from "./app.types";
+import { APP_TYPES } from './app.types';
 
 const INITIAL_STATE = {
   userLocation: {
-      lat: 43.6532,
-      lon: 79.3832
-  } 
+    lat: 43.6532,
+    lon: -79.3832
+  }
 };
 
-export const appReducer = (state = INITIAL_STATE, {type, payload}) => {
-    switch (type) {
-      case APP_TYPES.getUserLocationSuccess:
-        return {...state, ...{userLocation: {lat: payload["latitude"], lon: payload["longitude"]}}};
-      default:
-        return state;
-    }
-  };
-  
+export const appReducer = (state = INITIAL_STATE, { type, payload }) => {
+  switch (type) {
+    case APP_TYPES.getUserLocationSuccess:
+      return {
+        ...state,
+        ...{
+          userLocation: { lat: payload['latitude'], lon: payload['longitude'] }
+        }
+      };
+    default:
+      return state;
+  }
+};
