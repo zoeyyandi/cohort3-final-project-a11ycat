86c597e1fdf9d92df3e7d28008a5ded91a368ae7
Katie Davis <kdavis@league.com>
2018-03-27 18:38:11 -0400

    adds simple test, fixes jest issue, uncomments code (#26)

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,10 +1,14 @@
+/**
+ * @jest-environment
+ */
+
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { appReducer } from "./features/app/app.reducers";
 import AppContainer from "./features/app/app.container";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  ReactDOM.render(<AppContainer />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("Employers reducer", () => {
+  it("should be defined", () => {
+    expect(appReducer).toBeDefined();
+  });
 });
