49f12160bfe10c8b9f73d018abe7b0b49daecd9e
Marta Kule <marta.kule@gmail.com>
2018-04-05 18:51:00 -0400

    single quotes

--- client/src/features/location-rating/location-rating.component.js
+++ client/src/features/location-rating/location-rating.component.js
@@ -1,13 +1,13 @@
-import React, { Component } from "react";
-import { Button } from "../../ui-kit/button";
-import { Heading } from "../../ui-kit/heading";
+import React, { Component } from 'react';
+import { Button } from '../../ui-kit/button';
+import { Heading } from '../../ui-kit/heading';
 export class LocationRating extends Component {
   accessibilityFeatures = [
-    "Accessible parking",
-    "Ramp leading to the front door",
-    "Front door opens automatically",
-    "Signage is easily legible",
-    "Service animal is welcome"
+    'Accessible parking',
+    'Ramp leading to the front door',
+    'Front door opens automatically',
+    'Signage is easily legible',
+    'Service animal is welcome'
   ];
   handleOnClick = event => {
     event.preventDefault();
