4e03542ff1e38493524afa315f5ff33a4cb19c38
Rose W <rose.wan@me.com>
2018-04-06 09:34:02 -0400

    Hard coded additional pins to map (#43)
    * Hardcoded additional pins on map, zoom out to see all - this will depend on the location user is at
    
    * updated .env-example

--- client/src/features/google-map/google-map.component.js
+++ client/src/features/google-map/google-map.component.js
@@ -11,18 +11,48 @@ if (!googleMapsAPIKey)
   throw new Error('googleMapsAPIKey environment variable required');
 
 const Map = withScriptjs(
-  withGoogleMap(props => (
+  withGoogleMap(props => {
+    return (
     <GoogleMap
+      ref={props.onMapLoad}
       defaultZoom={15}
       defaultCenter={props.coords}
       gridSize={60}
+      onClick={props.onMapClick}
     >
-      <Marker position={props.coords} />
-    </GoogleMap>
-  ))
-);
+    <Marker title="User"
+      position={props.coords}
+    />
+    {props.markers.map((marker, index) => (
+      <Marker
+          {...marker}
+          onRightClick={() => props.onMarkerRightClick(marker)}
+          key={index}
+        position={marker.position}
+        onClick={() => props.onMarkerClick(marker)}
+      />
+    ))}
+    </GoogleMap>);
+  }
+));
 
 export class GoogleMapsComponent extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      markers:[{
+        position:{
+          lat: 43.6399,
+          lng: -79.3782,
+        }
+      }, {
+        position:{
+          lat: 43.639217,
+          lng: -79.400414,
+        }
+      }]
+    }
+  }
   render() {
     return (
       <Map
@@ -35,6 +65,7 @@ export class GoogleMapsComponent extends Component {
         containerElement={<div style={{ height: `600px` }} />}
         mapElement={<div style={{ height: `100%` }} />}
         coords={this.props.userCoords}
+        markers={this.state.markers}
       />
     );
   }
