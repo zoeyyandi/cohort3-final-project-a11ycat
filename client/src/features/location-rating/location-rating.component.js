4c94b056ee6dbf81364d7b6a0cd6c60b3f195c4d
katiavasilyeva <vasilyeva64@gmail.com>
2018-04-08 10:06:54 -0400

    initial css

--- client/src/features/location-rating/location-rating.component.js
+++ client/src/features/location-rating/location-rating.component.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react';
 import { Button } from '../../ui-kit/button';
 import { Heading } from '../../ui-kit/heading';
-import { Link } from 'react-router-dom';
+
+import styled from "styled-components";
+
+export const Item = styled.div`
+  float: left;
+  margin: 10px;
+  width: 350px;
+  length: 150px;
+`;
+
+export const Container = styled.div`
+  max-width: 1110px;
+  margin: 0 auto;
+  vertical-align: top;
+
+  @media  screen and (max-width: 1125px) {
+      max-width: 740px;
+      margin: 0 auto;
+  }
+  @media screen and (max-width: 755px) {
+    max-width: 370px;
+    margin: 0 auto;
+  }
+`;
+
+export const ButtonWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 export class LocationRating extends Component {
   accessibilityFeatures = [
     'Accessible parking',
@@ -26,9 +56,10 @@ export class LocationRating extends Component {
       <div>
         <Heading>{location.name}</Heading>
         <div>
+        <Container>
           {this.accessibilityFeatures.map((feature, index) => (
-            <div key={index}>
-              <label htmlFor={`rate${index}`}>
+            <Item key={index}>
+              <label htmlFor={index}>
                 <input
                   checked={savedToDb ? false : null}
                   type="checkbox"
@@ -40,13 +71,10 @@ export class LocationRating extends Component {
                 />
                 {feature}
               </label>
-            </div>
+            </Item>
           ))}
-        </div>
-        <div>
-          <Button onClick={this.handleOnClick}>
-              Submit
-          </Button>
+        </Container>
+        <Button onClick={this.handleOnClick}>Submit</Button>
         </div>
       </div>
     );
