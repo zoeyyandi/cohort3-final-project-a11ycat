4cf4449b606c64b88796b91e1dfad032450b4e10
Zoey Zheng <zoeyyandi@gmail.com>
2018-04-11 11:04:24 -0400

    fixed conflicts

--- client/src/features/google-map/google-map.reducers.js
+++ client/src/features/google-map/google-map.reducers.js
@@ -1,25 +1,16 @@
 import { GOOGLE_MAP_TYPES } from './google-map.types';
 
 const INITIAL_STATE = {
-<<<<<<< HEAD
-  ratedPlaces: [],
-  activeMarker: null
-=======
+  activeMarker: null,
   markers: []
->>>>>>> restructure data from db to markers, migrate markers from react state to redux
 };
 
 export const googleMapReducer = (state = INITIAL_STATE, { type, payload }) => {
   switch (type) {
-<<<<<<< HEAD
-    case GOOGLE_MAP_TYPES.updateRatedPlaces:
-      return { ...state, ratedPlaces: payload.ratedPlaces };
     case GOOGLE_MAP_TYPES.updateActiveMarker:
       return { ...state, activeMarker: payload.activeMarker };
-=======
     case GOOGLE_MAP_TYPES.updateMarkers:
       return { ...state, markers: payload.markers };
->>>>>>> restructure data from db to markers, migrate markers from react state to redux
     default:
       return state;
   }
