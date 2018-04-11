8b19b272854790536b7d0f5944cedc69d65b7d2f
Zoey Zheng <zoeyyandi@gmail.com>
2018-04-11 11:05:38 -0400

    fixed conflicts

--- client/src/features/google-map/google-map.types.js
+++ client/src/features/google-map/google-map.types.js
@@ -1,8 +1,4 @@
 export const GOOGLE_MAP_TYPES = {
-<<<<<<< HEAD
-  updateRatedPlaces: 'UPDATE_RATED_PLACES',
-  updateActiveMarker: 'UPDATE_ACTIVE_MARKER'
-=======
+  updateActiveMarker: 'UPDATE_ACTIVE_MARKER',
   updateMarkers: 'UPDATE_MARKERS'
->>>>>>> restructure data from db to markers, migrate markers from react state to redux
 };
